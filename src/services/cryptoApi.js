45a6c35e8e6ac8d41c87b7cbba1ff608535de41e
Ranja Func <seolin9340@gmail.com>
2021-10-04 01:19:07 +0900

    Add Chart

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -14,11 +14,18 @@ export const cryptoApi = createApi({
     baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: (builder) => ({
         getCryptos: builder.query({
-            query: (count) => createRequest(`/coins?limit=${count}`)
-        })
+            query: (count) => createRequest(`/coins?limit=${count}`),
+        }),
+        getExchanges: builder.query({
+            query: () => createRequest('/exchanges'),
+        }),
+        getCryptoDetails: builder.query({
+            query: (coinId) => createRequest(`/coin/${coinId}`),
+        }),
+        getCryptoHistory: builder.query({
+            query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history/${timeperiod}`),
+        }),
     })
 })
 
-export const {
-    useGetCryptosQuery
-} = cryptoApi;
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetExchangesQuery, useGetCryptoHistoryQuery } = cryptoApi;
